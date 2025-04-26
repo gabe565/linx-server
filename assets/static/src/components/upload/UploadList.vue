135cd45221122228aadb538bf63709d3e7043e3a
Gabe Cook <gabe565@gmail.com>
2025-04-26 02:21:58 -0500

    fix: fix upload list word-wrapping

--- assets/static/src/components/upload/UploadList.vue
+++ assets/static/src/components/upload/UploadList.vue
@@ -25,9 +25,9 @@
           </Card>
 
           <Card v-else class="py-4">
-            <CardHeader class="px-4 max-w-full">
-              <RouterLink :to="`/${item.filename}`" class="overflow-hidden">
-                <CardTitle class="wrap-break-word">
+            <CardHeader class="px-4">
+              <CardTitle class="min-w-0">
+                <RouterLink :to="`/${item.filename}`" class="wrap-break-word">
                   {{ item.filename }}
                   <span
                     v-if="item.original_name && item.filename !== item.original_name"
@@ -35,8 +35,8 @@
                   >
                     ({{ item.original_name }})
                   </span>
-                </CardTitle>
-              </RouterLink>
+                </RouterLink>
+              </CardTitle>
 
               <CardDescription v-if="item.expiry">
                 <UseTimeAgo
